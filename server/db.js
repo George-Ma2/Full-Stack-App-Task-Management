09d8d53978aa87e59b359be5eb55a94f76e6674b
George-Ma2 <george.ma@upr.edu>
2024-10-24 19:18:13 -0400

    Create db.js
    Created connection of database with server using pool

---
+++ server/db.js
@@ -0,0 +1,11 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+    user: "postgres",
+    password: "postgres2014",
+    host: "localhost",
+    port: 5432,
+    database: "sampledb"
+})
+
+module.exports = pool; 
--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require("./db")
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
+
+
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
