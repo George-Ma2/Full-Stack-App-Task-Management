2e8c0f089dfc594c1e9f0454f006ee985bc69475
George-Ma2 <george.ma@upr.edu>
2024-10-24 19:36:47 -0400

    Create post method to add task
    Create post method to add task. inserts into table tasks the description of the task

--- server/index.js
+++ server/index.js
@@ -6,7 +6,20 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+//create a task
+app.post("/tasks", async(req, res) => {
+    try {
+        const { description } = req.body
+        const newTask = await pool.query (
+            'INSERT INTO tasks (description) VALUES($1) RETURNING *',
+            [description]
+        )
 
+    // res.json(newTask.row[1])
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
