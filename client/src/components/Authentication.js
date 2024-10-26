64216c056a9d4f704730be3e3ec9de2a3bbd5db9
George-Ma2 <george.ma@upr.edu>
2024-10-26 14:52:59 -0400

    Create backend api for authentication
    Create backend api for authentication

--- client/src/components/Authentication.js
+++ client/src/components/Authentication.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Authentication = () => {
     return (
-    <>
-        <h1>Hello</h1>
-        {/* <div class="container">
-            <button class="btn btn-info mt-3 float-right">Authenticate</button>
-        </div> */}
-    </>)
-}
+        <div className="container text-center mt-5">
+            <h1>Welcome to the Task Management App</h1>
+            <h3>Please choose an option:</h3>
+            <div className="mt-4">
+                <Link to="/signin" className="btn btn-primary mx-2">
+                    Sign In
+                </Link>
+                <Link to="/login" className="btn btn-secondary mx-2">
+                    Log In
+                </Link>
+            </div>
+        </div>
+    );
+};
 
-export default Authentication
+export default Authentication;
