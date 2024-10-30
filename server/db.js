b94256bfc8f8ee7daf539c035b153a8f857489f6
George-Ma2 <george.ma@upr.edu>
2024-10-29 21:08:03 -0400

    Update db.js
    update host in databse

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "postgres",
     password: "postgres2014",
-    host: "https://full-stack-app-task-management.onrender.com",
+    host: "process.env.DB_HOST",
     port: 5432,
     database: "taskmanagement"
 })
