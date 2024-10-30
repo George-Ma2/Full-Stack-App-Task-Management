92401e6d87fcd19aad8d2f0914e7efb939aafe45
George-Ma2 <george.ma@upr.edu>
2024-10-29 23:25:25 -0400

    Update db.js

--- server/db.js
+++ server/db.js
@@ -6,6 +6,9 @@ const pool = new Pool({
     host: process.env.DB_HOST || 'dpg-csgonv3qf0us739o9dug-a.oregon-postgres.render.com',
     port: process.env.DB_PORT || 5432,
     database: process.env.DB_NAME || 'taskmanagement_9qqk',
+    ssl: {
+        rejectUnauthorized: false, // This is for development; consider changing in production
+    },
 })
 
 module.exports = pool;
