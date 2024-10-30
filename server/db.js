be48bae1236e1fd1182d311dc9e965ec7d8d65c7
George-Ma2 <george.ma@upr.edu>
2024-10-29 23:18:58 -0400

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: process.env.DB_USER || 'taskmanagement_9qqk_user',
     password: process.env.DB_PASSWORD || '28IBOCCmzXtAHzW1VX46UgqPplC4MO6T',
-    host: process.env.DB_HOST || 'dpg-csgonv3qf0us739o9dug-a',
+    host: process.env.DB_HOST || 'dpg-csgonv3qf0us739o9dug-a.oregon-postgres.render.com',
     port: process.env.DB_PORT || 5432,
     database: process.env.DB_NAME || 'taskmanagement_9qqk',
 })
