7a9a01aedb4eae0bdb414eaf63b266e78463dc23
George-Ma2 <george.ma@upr.edu>
2024-10-29 22:56:25 -0400

    update db configuration

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "taskmanagement_9qqk_user",
-    password: "28IBOCCmzXtAHzW1VX46UgqPplC4MO6T",
-    host: "dpg-csgonv3qf0us739o9dug-a",
-    port: 5432,
-    database: "taskmanagement_9qqk"
+    user: process.env.DB_USER || 'taskmanagement_9qqk_user',
+    password: process.env.DB_PASSWORD || '28IBOCCmzXtAHzW1VX46UgqPplC4MO6T',
+    host: process.env.DB_HOST || 'dpg-csgonv3qf0us739o9dug-a',
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_NAME || 'taskmanagement_9qqk',
 })
 
 module.exports = pool;
