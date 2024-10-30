cde189b4612451d47b258c690a3761816be43665
George-Ma2 <george.ma@upr.edu>
2024-10-29 22:40:55 -0400

    Update db.js
    update database configuration

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "postgres2014",
-    host: process.env.DB_HOST,
+    user: "taskmanagement_9qqk_user",
+    password: "28IBOCCmzXtAHzW1VX46UgqPplC4MO6T",
+    host: "dpg-csgonv3qf0us739o9dug-a",
     port: 5432,
-    database: "taskmanagement"
+    database: "taskmanagement_9qqk"
 })
 
 module.exports = pool;
